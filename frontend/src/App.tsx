7469e67f3ffcaa3d1bd9291500c92cc7b906e9d6
IdaGeoida <93858477+IdaGeoida@users.noreply.github.com>
2025-06-18 21:58:52 +0200

    Add GeneralForm component and App wrapper

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GeneralForm from './components/GeneralForm';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>General Form</h1>
+      <GeneralForm />
+    </div>
+  );
+};
+
+export default App;
